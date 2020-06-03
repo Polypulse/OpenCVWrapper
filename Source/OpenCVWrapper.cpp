9dae5c564b04972c246650b8e8173c9b535a0ed2
Sean Connor <sean@polypulse.io>
2020-06-02 21:30:34 -0700

    Added some copyright headers.

--- Source/OpenCVWrapper.cpp
+++ Source/OpenCVWrapper.cpp
@@ -1,3 +1,6 @@
+/* Copyright (C) Polypulse LLC - All Rights Reserved
+ * Written by Sean Connor <sean@polypulse.io>, April 2020 */
+
 #include "OpenCVWrapper.h"
 #include "MatQueueWriter.h"
 #include "WrapperLogQueue.h"
