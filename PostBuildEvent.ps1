2f6544ad347aa7387be07cfa0f7f004e1734a9aa
Sean Connor <sean@polypulse.io>
2020-11-29 20:58:12 -0700

    Refactored post build script to relative path.

--- PostBuildEvent.ps1
+++ PostBuildEvent.ps1
@@ -3,25 +3,28 @@ param([string]$projectdir,
       [string]$configuration,
       [string]$name);
 
-$targetpath = [IO.File]::ReadAllText(".\PostBuildTargetPath.txt")
-echo "Copying from: $outputpath to: $targetpath"
+# $targetpath = [IO.File]::ReadAllText(".\PostBuildTargetPath.txt")
+# echo "Copying from: $outputpath to: $targetpath"
 
 $includedir = "Include\"
-robocopy "$projectdir$includedir\" "$targetpath\Include\" /is
+robocopy "$projectdir$includedir\" "$projectdir\..\Include\" /is
 
-New-Item "$targetpath\Binaries\$configuration\Dynamic\" -ItemType Directory -ea 0
-New-Item "$targetpath\Binaries\$configuration\Static\" -ItemType Directory -ea 0
-New-Item "$targetpath\..\..\..\Binaries\ThirdParty\Win64\" -ItemType Directory -ea 0
-New-Item "$targetpath\..\..\..\Binaries\ThirdParty\Win64\" -ItemType Directory -ea 0
-New-Item "$targetpath\OpenCV\Include" -ItemType Directory -ea 0
+$copyPath = "$projectdir\..\Source\ThirdParty\OpenCVWrapper\"
 
-robocopy "$outputpath" "$targetpath\Binaries\$configuration\Dynamic\" "$name.dll" /is
-robocopy "$outputpath" "$targetpath\Binaries\$configuration\Dynamic\" "$name.pdb" /is
-robocopy "$outputpath" "$targetpath\Binaries\$configuration\Static\" "$name.lib" /is
-robocopy "$outputpath" "$targetpath\Binaries\$configuration\Static\" "$name.lib" /is
-robocopy "$projectdir\ThirdParty\OpenCV\Include\" "$targetpath\OpenCV\Include\" /E
+New-Item "$copyPath\Binaries\$configuration\Dynamic\" -ItemType Directory -ea 0
+New-Item "$copyPath\Binaries\$configuration\Static\" -ItemType Directory -ea 0
+New-Item "$copyPath\\Include" -ItemType Directory -ea 0
+New-Item "$copyPath\OpenCV\Include" -ItemType Directory -ea 0
 
-robocopy "$outputpath" "$targetpath\..\..\..\Binaries\ThirdParty\Win64\" "$name.dll" /is
+robocopy "$outputpath" "$copyPath\Binaries\$configuration\Dynamic\" "$name.dll" /is
+robocopy "$outputpath" "$copyPath\Binaries\$configuration\Dynamic\" "$name.pdb" /is
+robocopy "$outputpath" "$copyPath\Binaries\$configuration\Static\" "$name.lib" /is
+robocopy "$outputpath" "$copyPath\Binaries\$configuration\Static\" "$name.lib" /is
+robocopy "$outputpath" "$copyPath\Binaries\$configuration\Static\" "$name.lib" /is
+robocopy "$projectdir\Include" "$copyPath\Include\"
+robocopy "$projectdir\ThirdParty\OpenCV\Include\" "$copyPath\OpenCV\Include\" /E
+
+robocopy "$outputpath" "$projectdir\..\..\..\..\Binaries\ThirdParty\Win64\" "$name.dll" /is
 if ($configuration -eq "Debug") {
-    robocopy "$outputpath" "$targetpath\..\..\..\Binaries\ThirdParty\Win64\" "$name.pdb" /is
+    robocopy "$outputpath" "$projectdir\..\..\..\..\Binaries\ThirdParty\Win64\" "$name.pdb" /is
 }
