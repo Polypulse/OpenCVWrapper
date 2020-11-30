5ab9bab89b61434303596b9a1bb7d3bf6869f64d
Sean Connor <sean@polypulse.io>
2020-11-29 20:46:16 -0700

    Updated build script.

--- PostBuildEvent.ps1
+++ PostBuildEvent.ps1
@@ -13,10 +13,13 @@ New-Item "$targetpath\Binaries\$configuration\Dynamic\" -ItemType Directory -ea 
 New-Item "$targetpath\Binaries\$configuration\Static\" -ItemType Directory -ea 0
 New-Item "$targetpath\..\..\..\Binaries\ThirdParty\Win64\" -ItemType Directory -ea 0
 New-Item "$targetpath\..\..\..\Binaries\ThirdParty\Win64\" -ItemType Directory -ea 0
+New-Item "$targetpath\OpenCV\Include" -ItemType Directory -ea 0
 
 robocopy "$outputpath" "$targetpath\Binaries\$configuration\Dynamic\" "$name.dll" /is
 robocopy "$outputpath" "$targetpath\Binaries\$configuration\Dynamic\" "$name.pdb" /is
 robocopy "$outputpath" "$targetpath\Binaries\$configuration\Static\" "$name.lib" /is
+robocopy "$outputpath" "$targetpath\Binaries\$configuration\Static\" "$name.lib" /is
+robocopy "$projectdir\ThirdParty\OpenCV\Include\" "$targetpath\OpenCV\Include\" /E
 
 robocopy "$outputpath" "$targetpath\..\..\..\Binaries\ThirdParty\Win64\" "$name.dll" /is
 if ($configuration -eq "Debug") {
